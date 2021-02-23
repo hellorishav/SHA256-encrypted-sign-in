ee6989ffbb7003e631a2c771ad87997381ba8569
Rishav Kumar <hello@rishavkumar.io>
2021-02-23 18:30:14 +0530

    Update sign-in.php

--- sign-in.php
+++ sign-in.php
@@ -29,17 +29,17 @@ if (isset($_POST["submit"])) {
         die("Connection failed: " . $conn->connect_error);
       }
 
-      $sql = "SELECT from user_data where username like '".$user_username."' and password like '".$user_password."'";
-      $result = mysqli_query($conn, $sql);  
+      $sql = "select *from login where username = '$user_username' and password = '$user_password'";  
+      $result = mysqli_query($con, $sql);  
       $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
       $count = mysqli_num_rows($result);  
-          
+        
       if($count == 1){  
-        echo "<h1><center> Login successful </center></h1>";  
+          echo "<h1><center> Login successful </center></h1>";  
       }  
       else{  
-        echo "<h1> Login failed. Invalid username or password.</h1>";  
-      }
+          echo "<h1> Login failed. Invalid username or password.</h1>";  
+      }     
 
       $conn->close();
   
