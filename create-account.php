d5af4c24d0a49deeb61350a977937d0e4b9496ea
Rishav Kumar <hello@rishavkumar.io>
2021-02-23 14:46:45 +0530

    Update create-account.php

--- create-account.php
+++ create-account.php
@@ -10,20 +10,15 @@
         $password = "SHA256@encrypt";
         $dbname = "u947421468_SHA256_SignIn";
 
-        // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
+
         if ($conn->connect_error) {
           die("Connection failed: " . $conn->connect_error);
         }
 
         $sql = "INSERT INTO `user_data`(`username`, `password`) VALUES ($username, $password)";
 
-        if ($conn->query($sql) === TRUE) {
-          echo "New record created successfully";
-        } else {
-          echo "Error: " . $sql . "<br>" . $conn->error;
-        }
+        $conn->query($sql);
 
         $conn->close();
 
